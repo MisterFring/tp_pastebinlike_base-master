b5dc0352109918946bfa20f9946642ac51b35b25
Pierre <pierre.decrock@gmail.com>
2020-10-30 14:06:56 +0100

    v final

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,4 +1,4 @@
-crypto = require('crypto')
+const crypto = require('crypto')
 
 module.exports = function createUserController(db) {
 
@@ -15,10 +15,14 @@ module.exports = function createUserController(db) {
                     pseudo : alreadyPseudo
                 }
             }
+            const hash = crypto.createHash('sha256');
+            hash.update(password);
+            const lehash = hash.digest('hex')
+            console.log("mdp hashé : " + lehash);
 
             // ne pas stocker les mot de passe en clair !
             await users.insertOne({
-                email: email, pseudo: pseudo, password: password
+                email: email, pseudo: pseudo, password: lehash
             })
 
             return { success: true }
@@ -26,7 +30,10 @@ module.exports = function createUserController(db) {
 
         async login( email, password ) {
             const user = await users.findOne({ email: email })
-            if (!(user && user.password === password)) {
+            const hash = crypto.createHash('sha256');
+            hash.update(password);
+            const lehash = hash.digest('hex');
+            if (!(user && user.password === lehash)) {
                 return { error: 'Bad credentials' }
             }
             user.authToken = crypto.randomBytes(20).toString('hex')
